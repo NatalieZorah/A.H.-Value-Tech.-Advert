aced6d17db8c55f7aa5455056c32dcebf9a1ef04
Natalie Evans <NatalieRose93019@gmail.com>
2022-08-26 21:05:14 -0500

    added media quearying to accommodate mobile, landscape, and desktop

--- src/components/static elements/Header.jsx
+++ src/components/static elements/Header.jsx
@@ -6,8 +6,8 @@ import "../../styles/static elements/Header.css";
 const Header = () => {
   return (
     <div className="header-wrapper">
-      <h1>ANN HIRO VALUE TECHNOLOGY</h1>
-      <h3>YOUR PC ON A BUDGET</h3>
+      <h1 className="font-type-header">ANN HIRO VALUE TECHNOLOGY</h1>
+      <h3 className="font-type-header">YOUR PC ON A BUDGET</h3>
     </div>
   );
 };
